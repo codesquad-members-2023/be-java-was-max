6dfa9b5a0f159ba9d7f1911c275a6e8ce9f9837e
Sully <2dcoder@naver.com>
2023-05-08 15:25:10 +0900

    feat: 모든 Request Header 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -22,6 +22,20 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            // https://docs.oracle.com/javase/8/docs/api/java/nio/charset/StandardCharsets.html
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            // 라인별로 http header 읽기
+            String line = br.readLine();
+
+            // 어떤 스레드, 클래스에서 해당 로그를 출력하는지까지 다 출력
+            log.debug("request line : {}", line);
+
+            // request 마지막에 빈 공백 문자열이 들어오니 그때까지 반복
+            while (!line.equals("")) {
+                line = br.readLine();
+                log.debug("header : {}", line);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
