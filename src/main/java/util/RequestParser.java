70bfcfc9bc46d5e02c2ad86b3c8a7ff5a9ac3090
n-aa-g <wlsdnjs0124@gmail.com>
2023-05-09 16:43:18 +0900

    Feat(RequestParser): 클래스의 파싱 메서드 변경
    Header의 startLine을 파싱하는 parseStartLine 메서드와 그 외의 header를 파싱하는 parseHeader메서드를 만들었습니다.

--- src/main/java/util/RequestParser.java
+++ src/main/java/util/RequestParser.java
@@ -1,8 +1,28 @@
 package util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RequestParser {
 
-    public static String[] createTokens(String requestLine) {
-        return requestLine.split(" ");
+    public static Map<String, String> parseStartLine (String startLine) {
+        Map<String, String> parsedStartLine = new HashMap<>();
+        String[] split = startLine.split(" ");
+
+        parsedStartLine.put("Method", split[0]);
+        parsedStartLine.put("Url", split[1]);
+        parsedStartLine.put("Protocol", split[2]);
+        return parsedStartLine;
+    }
+
+    public static Map<String, String> parseHeader (String header) {
+        Map<String, String> parsedHeader = new HashMap<>();
+        String[] splitLine = header.split(System.lineSeparator());
+
+        for (String line : splitLine) {
+            String[] split = line.split(": ");
+            parsedHeader.put(split[0], split[1]);
+        }
+        return parsedHeader;
     }
 }
