5714ab6842388d65bae8a3fe8c4d57543413a5c2
yeonise <cerealnum@gmail.com>
2023-05-09 10:19:40 +0900

    refactor : 프로젝트 분석

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,19 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
 
@@ -19,28 +22,28 @@ public class RequestHandler implements Runnable {
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+        LOGGER.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // TODO 사용자 요청 처리 구현
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
-            logger.error(e.getMessage());
+            LOGGER.error(e.getMessage());
         }
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("HTTP/1.1 200 OK\r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
-            logger.error(e.getMessage());
+            LOGGER.error(e.getMessage());
         }
     }
 
@@ -49,7 +52,7 @@ public class RequestHandler implements Runnable {
             dos.write(body, 0, body.length);
             dos.flush();
         } catch (IOException e) {
-            logger.error(e.getMessage());
+            LOGGER.error(e.getMessage());
         }
     }
 }
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -7,25 +7,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WebServer {
-    private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
-    private static final int DEFAULT_PORT = 8080;
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebServer.class);
+    private static final int DEFAULT_PORT = 8080; // 기본 포트 값 설정
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
+    public static void main(String[] args) throws Exception {
+        int port = 0; // port 0으로 초기화
+        if (args == null || args.length == 0) { // 인자가 없는 경우
+            port = DEFAULT_PORT; // DEFAULT_PORT 상수 값을 port에 대입
+        } else { // 인자가 하나 이상 있는 경우
+            port = Integer.parseInt(args[0]); // args[0]에 저장된 값을 Integer.parseInt() 메서드를 이용하여 정수형으로 변환한 뒤 port 변수에 대입
         }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
+        // 서버 소켓을 생성
+        // 웹 서버는 기본적으로 8080 포트를 사용
         try (ServerSocket listenSocket = new ServerSocket(port)) {
-            logger.info("Web Application Server started {} port.", port);
+            LOGGER.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
+            // 클라이언트와 연결될 때까지 대기
             Socket connection;
-            while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
+            while ((connection = listenSocket.accept()) != null) { // accept() 메서드를 이용하여 클라이언트의 연결 요청을 대기하다가 연결 요청이 들어오면 연결을 수락하고 클라이언트와 통신할 소켓인 connection 객체를 생성
+                // accept() 메서드를 호출하는 부분에서 Blocking 상태가 되며 클라이언트의 연결 요청이 들어오기 전까지 대기
+                Thread thread = new Thread(new RequestHandler(connection)); // connection 객체를 이용하여 새로운 RequestHandler 객체를 생성한 뒤 새로운 스레드에게 할당
                 thread.start();
             }
         }
