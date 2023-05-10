ab669efa79a99ccfffe8980b1a333f14b18aa4d1
Sully <2dcoder@naver.com>
2023-05-10 10:35:16 +0900

    refactor: assertEquals -> assertThat
    이게 더 가독성이 좋기도 하고, Junit 4.4부터 이 메서드가 추가됐으니 그냥 이걸로 쓰자

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -3,6 +3,7 @@ package webserver;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpRequestTest {
@@ -11,18 +12,18 @@ class HttpRequestTest {
     void testSeparateUrl() {
         // 기본 URL 처리
         String url1 = HttpRequest.separateUrl("GET /index.html HTTP/1.1");
-        assertEquals("/index.html", url1);
+        assertThat(url1).isEqualTo("/index.html");
 
         // URL에 파라미터가 포함된 경우
         String url2 = HttpRequest.separateUrl("GET /index.html?id=sully HTTP/1.1");
-        assertEquals("/index.html", url2);
+        assertThat(url2).isEqualTo("/index.html");
 
         // URL에 경로가 포함된 경우
         String url3 = HttpRequest.separateUrl("GET /user/sully123 HTTP/1.1");
-        assertEquals("/user/sully123", url3);
+        assertThat(url3).isEqualTo("/user/sully123");
 
         // URL이 "/"인 경우
         String url4 = HttpRequest.separateUrl("GET / HTTP/1.1");
-        assertEquals("/index.html", url4);
+        assertThat(url4).isEqualTo("/index.html");
     }
 }
