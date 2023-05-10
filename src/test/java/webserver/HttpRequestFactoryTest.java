a8b98ac2d1841732b5a4a4b3eb0df686e74917f1
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-05-10 15:12:10 +0900

    test: softAssertions으로 변경

--- src/test/java/webserver/HttpRequestFactoryTest.java
+++ src/test/java/webserver/HttpRequestFactoryTest.java
@@ -1,38 +1,38 @@
 package webserver;
 
+import static webserver.request.common.HttpMethod.*;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import webserver.request.HttpRequest;
-import webserver.request.HttpRequestFactory;
-import webserver.request.RequestLine;
-import webserver.request.RequestParameter;
+import webserver.request.factory.HttpRequestFactory;
 
 class HttpRequestFactoryTest {
 
     @Test
     @DisplayName("Inputstream이 주어지고 HttpRequestFactory에 전달하여 HttpRequest 객체를 생성한다")
-    public void createHttpRequest() throws IOException {
-        //given
-        String requestLine = "GET /sample.html HTTP/1.0";
-        String host = "Host: localhost";
-        String eof = "\r\n";
-        String requestHeader = String.join("\r\n", requestLine, host, eof);
-        InputStream in = new ByteArrayInputStream(requestHeader.getBytes());
-        //when
-        HttpRequest httpRequest =
-            HttpRequestFactory.createHttpRequest(new BufferedReader(new InputStreamReader(in)));
-        //then
-
-        Assertions.assertThat(httpRequest.getRequestLine().getMethod()).isEqualTo("GET");
-        Assertions.assertThat(httpRequest.getRequestLine().getRequestURI())
-            .isEqualTo("/sample.html");
-        Assertions.assertThat(httpRequest.getRequestLine().getHttpVersion()).isEqualTo("HTTP/1.0");
-        Assertions.assertThat(httpRequest.getHeader().get("Host")).isEqualTo("localhost");
+    void createHttpRequest() throws IOException {
+        // given
+        String requestHeader = String.join("\r\n",
+            "GET /sample.html HTTP/1.0",
+            "Host: localhost",
+            "\r\n");
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(new ByteArrayInputStream(requestHeader.getBytes())));
+        // when
+        HttpRequest httpRequest = HttpRequestFactory.create(br);
+        // then
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(httpRequest.getHttpMethod()).isEqualTo(GET);
+        softAssertions.assertThat(httpRequest.getRequestURI().getPath()).isEqualTo("/sample.html");
+        softAssertions.assertThat(httpRequest.getHttpVersion().toString()).isEqualTo("HTTP/1.0");
+        softAssertions.assertThat(httpRequest.getHeader("Host"))
+            .isEqualTo("localhost");
+        softAssertions.assertAll();
     }
 }
