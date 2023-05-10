64d75bd954b46951773a7b0c3c189ccbae308cc1
Sully <2dcoder@naver.com>
2023-05-10 22:26:12 +0900

    feat: Request Parameter 추출 구현
    Header의 첫 번째 라인에서 요청 URL을 추출

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -1,16 +1,49 @@
 package webserver;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HttpRequest {
     public HttpRequest() {
     }
 
     public static String separateUrl(String line) {
-        String url = line.split(" ")[1].split("\\?")[0];
+        String url = extractUrl(line);
+
+        if (url.contains("?")) {
+            url = url.split("\\?")[0];
+        }
 
         if (url.equals("/")) {
             url = "/index.html";
         }
 
         return url;
+    }
+
+    public static String separateMethod(String line) {
+        return line.split(" ")[0];
+    }
+
+    public static Map<String, String> separateParam(String line) {
+        String url = extractUrl(line);
+
+        if (!url.contains("?")) {
+            return null;
+        }
+
+        String[] params = url.split("\\?")[1].split("&");
+
+        Map<String, String> paramMap = new HashMap<>();
+        for (String param : params) {
+            String[] keyValue = param.split("=");
+            paramMap.put(keyValue[0], keyValue[1]);
+        }
+
+        return paramMap;
+    }
+
+    private static String extractUrl(String line) {
+        return line.split(" ")[1];
     }
 }
