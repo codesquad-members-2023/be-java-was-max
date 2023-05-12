dfb862299150bab0923dbcf19e585a0ffd1c1c1e
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-05-12 12:55:00 +0900

    feat: Http에서 공통적으로 사용하는 상수 정의
    * ContentType, HttpMethod, HttpStatus, 프로토콜버전등이 정의되어 있습니다.

---
+++ src/main/java/http/common/ProtocolVersion.java
@@ -0,0 +1,52 @@
+package http.common;
+
+import java.util.Objects;
+
+public class ProtocolVersion {
+
+    private final String protocol;
+    private final int major;
+    private final int minor;
+
+    public ProtocolVersion(String protocol, int major, int minor) {
+        this.protocol = protocol;
+        this.major = major;
+        this.minor = minor;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public int getMajor() {
+        return major;
+    }
+
+    public int getMinor() {
+        return minor;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getProtocol(), getMajor(), getMinor());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ProtocolVersion)) {
+            return false;
+        }
+        ProtocolVersion that = (ProtocolVersion) o;
+        return getMajor() == that.getMajor() && getMinor() == that.getMinor()
+            && getProtocol().equals(
+            that.getProtocol());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%d.%d", protocol, major, minor);
+    }
+}
