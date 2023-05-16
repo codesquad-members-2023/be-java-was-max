cb227acf1e74c0e76ae059ac6e9d4df5d38599e5
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-05-16 15:41:27 +0900

    chore: 코드 정리

--- src/main/java/webserver/handler/StaticResourceHandler.java
+++ src/main/java/webserver/handler/StaticResourceHandler.java
@@ -1,12 +1,14 @@
 package webserver.handler;
 
-import static http.common.ContentType.of;
 import static http.common.HttpStatus.OK;
-import static http.common.HttpVersion.HTTP_1_1;
+import static http.common.header.EntityHeaderType.CONTENT_LENGTH;
+import static http.common.header.EntityHeaderType.CONTENT_TYPE;
+import static http.common.version.HttpVersion.HTTP_1_1;
+import static http.response.component.ContentType.resolve;
 
-import http.common.ContentType;
-import http.response.HttpServletResponse;
-import http.response.StatusLine;
+import http.response.HttpResponse;
+import http.response.component.ContentType;
+import http.response.component.StatusLine;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,12 +19,12 @@ public class StaticResourceHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(StaticResourceHandler.class);
 
-    public void process(File file, final HttpServletResponse response) throws IOException {
+    public void process(File file, final HttpResponse response) throws IOException {
         byte[] messageBody = Files.readAllBytes(file.toPath());
-        ContentType contentType = of(file.getPath());
+        ContentType contentType = resolve(file.getPath());
         response.setStatusLine(new StatusLine(HTTP_1_1, OK));
-        response.addHeader("Content-Type", contentType);
-        response.addHeader("Content-Length", String.valueOf(messageBody.length));
+        response.addHeader(CONTENT_TYPE, contentType.toString());
+        response.addHeader(CONTENT_LENGTH, String.valueOf(messageBody.length));
         response.setMessageBody(messageBody);
     }
 }
