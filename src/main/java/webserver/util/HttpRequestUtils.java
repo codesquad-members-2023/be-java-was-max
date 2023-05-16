1fa48df127cdd4bdd9ddc122df84ca43520a621b
Sully <2dcoder@naver.com>
2023-05-16 21:47:19 +0900

    refactor: 상수로 만들기

--- src/main/java/webserver/util/HttpRequestUtils.java
+++ src/main/java/webserver/util/HttpRequestUtils.java
@@ -8,6 +8,11 @@ import java.util.Map;
 public class HttpRequestUtils {
     private static final int METHOD_INDEX = 0;
     private static final int URL_INDEX = 1;
+    private static final String BLANK = " ";
+    private static final String QUESTION_MARK = "?";
+    private static final String AMPERSAND = "&";
+    private static final String EQUALS_MARK = "=";
+
 
     private HttpRequestUtils() {
     }
@@ -19,7 +24,7 @@ public class HttpRequestUtils {
     public static String parseUrl(String requestLine) {
         String url = extractUrl(requestLine);
 
-        if (url.contains("?")) {
+        if (url.contains(QUESTION_MARK)) {
             url = url.split("\\?")[METHOD_INDEX];
         }
 
@@ -31,21 +36,21 @@ public class HttpRequestUtils {
     }
 
     public static String parseMethod(String requestLine) {
-        return requestLine.split(" ")[METHOD_INDEX];
+        return requestLine.split(BLANK)[METHOD_INDEX];
     }
 
     public static Map<String, String> parseQueryString(String requestLine) {
         String url = extractUrl(requestLine);
 
-        if (!url.contains("?")) {
+        if (!url.contains(QUESTION_MARK)) {
             return null;
         }
 
-        String[] params = url.split("\\?")[URL_INDEX].split("&");
+        String[] params = url.split("\\?")[URL_INDEX].split(AMPERSAND);
 
         Map<String, String> queryMap = new HashMap<>();
         for (String param : params) {
-            String[] keyValue = param.split("=");
+            String[] keyValue = param.split(EQUALS_MARK);
             queryMap.put(keyValue[METHOD_INDEX], keyValue[URL_INDEX]);
         }
 
@@ -53,6 +58,6 @@ public class HttpRequestUtils {
     }
 
     private static String extractUrl(String requestLine) {
-        return requestLine.split(" ")[URL_INDEX];
+        return requestLine.split(BLANK)[URL_INDEX];
     }
 }
