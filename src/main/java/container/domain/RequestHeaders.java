a35068fd9ff77fe93b06c0f1f04c48cf042f0502
ALbertIM0427 <guang7226961@naver.com>
2023-05-16 17:29:42 +0900

    feat: RequestHeaders 추가

---
+++ src/main/java/container/domain/RequestHeaders.java
@@ -0,0 +1,11 @@
+package container.domain;
+
+import java.util.List;
+
+public class RequestHeaders {
+    private final List<Header> headers;
+
+    public RequestHeaders(List<Header> headers) {
+        this.headers = headers;
+    }
+}
