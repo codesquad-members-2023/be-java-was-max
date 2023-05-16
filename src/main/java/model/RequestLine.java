350cb58aaba45984bdbbf29abb90f2ad892ff11d
Sully <2dcoder@naver.com>
2023-05-16 22:37:20 +0900

    test: RequestLine에서도 RequestUtils 기능이 되는지

--- src/main/java/model/RequestLine.java
+++ src/main/java/model/RequestLine.java
@@ -6,10 +6,12 @@ public class RequestLine {
     private final String method;
     private final ContentType contentType;
     private final String path;
+    private final String url;
     private final Map<String, String> queryMap;
 
     public RequestLine(String method, String url, Map<String, String> queryMap) {
         this.method = method;
+        this.url = url;
         this.contentType = ContentType.findByUrl(url);
         this.path = contentType.separatePath(url);
         this.queryMap = queryMap;
@@ -29,5 +31,9 @@ public class RequestLine {
 
     public String getPath() {
         return path;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }
--- src/test/java/model/RequestLineTest.java
+++ src/test/java/model/RequestLineTest.java
@@ -2,31 +2,24 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Map;
+import webserver.util.HttpRequestUtils;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RequestLineTest {
     @Test
-    @DisplayName("css 경로 반환 확인")
-    void testRequestCss() {
-        String url = "/css/bootstrap.mis.css";
-        String expectedUrl = "src/main/resources/static" + url;
-        RequestLine requestLine = new RequestLine("", url, Map.of());
-
-        assertThat(expectedUrl).isEqualTo(requestLine.separateAbsolutePath());
-        assertThat("css").isEqualTo(requestLine.separateRequestType());
+    @DisplayName("요청 method가 GET이다.")
+    void testParseMethod() {
+        String request = "GET / HTTP/1.1";
+        RequestLine requestLine = new RequestLine(HttpRequestUtils.parseMethod(request), HttpRequestUtils.parseUrl(request), HttpRequestUtils.parseQueryString(request));
+        assertThat(requestLine.getMethod()).isEqualTo("GET");
     }
 
     @Test
-    @DisplayName("html 경로 반환 확인")
-    void testRequestHtml() {
-        String url = "/";
-        String expectedUrl = "src/main/resources/templates" + "/index.html";
-        RequestLine requestLine = new RequestLine("", url, Map.of());
-
-        assertThat(expectedUrl).isEqualTo(requestLine.separateAbsolutePath());
-        assertThat("html").isEqualTo(requestLine.separateRequestType());
+    @DisplayName("요청 url이 index다.")
+    void testParseUrl() {
+        String request = "GET /index.html HTTP/1.1";
+        RequestLine requestLine = new RequestLine(HttpRequestUtils.parseMethod(request), HttpRequestUtils.parseUrl(request), HttpRequestUtils.parseQueryString(request));
+        assertThat(requestLine.getUrl()).isEqualTo("/index.html");
     }
 }
