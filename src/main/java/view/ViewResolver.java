f6cfc2ff41c26995e8d5d365cd6017bafc7014dc
sudago <sudago22@gmail.com>
2023-05-19 17:06:19 +0900

    refactor: ViewResolver 에게 파일 경로 판별하는 기능 위임

--- src/main/java/view/ViewResolver.java
+++ src/main/java/view/ViewResolver.java
@@ -1,31 +1,45 @@
 package view;
 
 import http.ContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.Paths;
 
 public class ViewResolver {
-    private final String absolutePath = Paths.get("").toAbsolutePath().toString();
-    private final String classPath = absolutePath + "/src/main/resources";
-    private final String staticPath = classPath + "/static";
-    private final String templatesPath = classPath + "/templates";
-    private String contentType = null;
-    private String totalPath = null;
+    private static final Logger logger = LoggerFactory.getLogger(ViewResolver.class);
+    private final String ABSOLUTE_PATH = Paths.get("").toAbsolutePath().toString();
+    private final String ROOT_PATH = ABSOLUTE_PATH + "/";
+    private final String RESOURCES_PATH = ROOT_PATH + "src/main/resources";
+    private final String STATIC_PATH = RESOURCES_PATH + "/static";
+    private final String TEMPLATES_PATH = RESOURCES_PATH + "/templates";
 
+    // TODO: redirect 처리 추가 -> location header에 추가
+    // TODO: RequestHandler 클래스의 response200Header 메서드 다루기
+    // 순서 고민: user/create를 하면, user를 생성하고 해당하는 html을 찾아서 반환하는게 맞는가?(반환할 수 있는 html 파일이 없다면?)
 
     public String run(String viewPath){
-        this.contentType = ContentType.get(viewPath);
-        // TODO: static 먼저 탐방하고, 없으면 template에서 찾고, 없으면 에러를 발생하게끔 수정하기
-        //  해당 file 부터 찾게, 알맞은 클래스를 찾아서 위치 수정하기
-        findTotalPath(viewPath);
+        return findTotalPath(viewPath);
     }
-    private void findTotalPath(String viewPath){
-        // TODO: static 폴더 부터 찾고, 없으면 templates 폴더 에서 찾기.
-        // 둘 다 없으면 에러 발생
-        staticPath + viewPath;
-        templatesPath + viewPath;
 
+    private String findTotalPath(String viewPath){
+        // static 폴더 부터 찾고, 없으면 templates 폴더 에서 찾기.
+        // 둘 다 없으면 에러 발생
+        if (isFileExist(STATIC_PATH + viewPath)) {
+            return STATIC_PATH + viewPath;
+        }
+        if (isFileExist(TEMPLATES_PATH + viewPath)){
+            return TEMPLATES_PATH + viewPath;
+        }
+        throw new RuntimeException("파일이 존재하지 않습니다. Path: " + viewPath);
+//        throw new FileNotFoundException("파일이 존재하지 않습니다. Path: " + viewPath);
     }
-    getContentType()
-    getTotalPath()
+
+    private boolean isFileExist(String path){
+        logger.debug("path: {}", path);
+        return new File(path).exists();
+    }
 }
