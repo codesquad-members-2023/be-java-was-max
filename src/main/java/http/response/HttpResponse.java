7f4005c9a4674f193ec5e042e4719df5af50c7d1
sudago <sudago22@gmail.com>
2023-05-22 20:09:20 +0900

    feature: response의 전체 log 출력

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -1,8 +1,13 @@
 package http.response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Map;
+import java.util.Optional;
 
 public class HttpResponse {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private StatusLine statusLine;
     private final ResponseHeaders responseHeaders;
     private final ResponseBody responseBody;
@@ -34,5 +39,35 @@ public class HttpResponse {
 
     public byte[] getResponseBody() {
         return responseBody.get();
+    }
+
+    public Optional<String> getContentType(){
+        return responseHeaders.get("Content-Type");
+    }
+
+    public Optional<String> getContentLength(){
+        return responseHeaders.get("Content-Length");
+    }
+
+    public String getStatusCode(){
+        return statusLine.getStatusCode();
+    }
+
+    public Optional<String> getLocation() {
+        return responseHeaders.get("Location");
+    }
+
+    public void logResponse(){
+        logger.debug("✅*******Response*******");
+        logger.debug("<Status Line>");
+        logger.debug(statusLine.get()); // requestLine
+        logger.debug("<Response Headers>");
+        Map<String, String> responseHeadersMap = responseHeaders.get();
+        for (String key: responseHeadersMap.keySet()){
+            logger.debug("{}: {}", key, responseHeadersMap.get(key)); // requestHeader
+        }
+        // TODO: 바이트 코드 문자열로 수정하기
+//        logger.debug("<Response Body>");
+//        logger.debug(Arrays.toString(responseBody.get())); // requestBody
     }
 }
