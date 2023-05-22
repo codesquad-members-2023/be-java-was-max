7cc89b3cf060785d036ae8d8472af3fc0dc0d936
CDBchan <kdjin1215@naver.com>
2023-05-22 22:56:17 +0900

    feat: 동적으로 html 의 header,navbar 추가 기능 구현

--- src/main/java/servlet/view/impl/OkView.java
+++ src/main/java/servlet/view/impl/OkView.java
@@ -11,9 +11,26 @@ public class OkView implements View {
 	@Override
 	public byte[] render(String viewPath) {
 		try {
-			return Files.readAllBytes(new File(viewPath).toPath());
+			if (viewPath.contains("static")) {
+				return Files.readAllBytes(new File(viewPath).toPath());
+			}
+			StringBuilder html = new StringBuilder(new String(Files.readAllBytes(new File(viewPath).toPath())));
+			return renderDynamicContent(html);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private byte[] renderDynamicContent(StringBuilder html) throws IOException {
+		replacePlaceholderWithFileContent(html, "{{> header}}", "src/main/resources/templates/fragments/header.html");
+		replacePlaceholderWithFileContent(html, "{{> navbar}}", "src/main/resources/templates/fragments/navbar.html");
+		return html.toString().getBytes();
+	}
+
+	private void replacePlaceholderWithFileContent(StringBuilder html, String placeholder, String filePath) throws
+		IOException {
+		String fileContent = new String(Files.readAllBytes(new File(filePath).toPath()));
+		int index = html.indexOf(placeholder);
+		html.replace(index, index + placeholder.length(), fileContent);
 	}
 }
