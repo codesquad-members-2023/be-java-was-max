1b75d01969af353aa100236e1aff97fd1eaf77ad
yeonise <cerealnum@gmail.com>
2023-05-22 15:25:28 +0900

    refactor : 정규표현식을 상수로 추가
    - QUERY_STRING_REGEX : URL과 QueryString을 split 할 때 사용하는 정규표현식입니다.

--- src/main/java/http/request/RequestLine.java
+++ src/main/java/http/request/RequestLine.java
@@ -3,25 +3,26 @@ package http.request;
 import http.HttpMethod;
 import http.HttpUtils;
 
-public class HttpRequestLine {
+public class RequestLine {
     private static final int METHOD_INDEX = 0;
     private static final int URL_INDEX = 1;
     private static final int PROTOCOL_INDEX = 2;
     private static final String  START_LINE_DIVIDER = " ";
     private static final String QUERY_STRING = "?";
+    private static final String QUERY_STRING_REGEX = "\\?";
     private final HttpMethod method;
     private final String url;
     private QueryString queryString;
     private final String protocol;
 
-    public HttpRequestLine(String startLine) {
+    public RequestLine(String startLine) {
         String[] tokens = startLine.split(START_LINE_DIVIDER);
         this.method = HttpUtils.getMethodType(tokens[METHOD_INDEX]);
         this.protocol = tokens[PROTOCOL_INDEX];
 
         if (startLine.contains(QUERY_STRING)) { // if queryString exists
-            this.url = tokens[URL_INDEX].split("\\?")[0];
-            this.queryString = new QueryString(tokens[1].split("\\?")[1]);
+            this.url = tokens[URL_INDEX].split(QUERY_STRING_REGEX)[0];
+            this.queryString = new QueryString(tokens[1].split(QUERY_STRING_REGEX)[1]);
             return;
         }
 
