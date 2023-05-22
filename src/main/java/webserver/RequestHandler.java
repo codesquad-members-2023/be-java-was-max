9e1d2d38a962a500231a6a30e41306099a601729
yeonise <cerealnum@gmail.com>
2023-05-23 08:37:30 +0900

    refactor : ResponseHeader와 ResponseBody의 2가지 메서드를 갖도록 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,11 @@
 package webserver;
 
-import controller.UserController;
-import http.HttpBody;
+import controller.FrontController;
 import http.HttpHeader;
-import http.HttpUtils;
 import http.request.HttpRequest;
-import http.request.HttpRequestLine;
+import http.request.RequestBody;
+import http.request.RequestLine;
+import http.response.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,13 +16,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.google.common.net.HttpHeaders.CONTENT_LENGTH;
+import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
+import static com.google.common.net.HttpHeaders.LOCATION;
+import static com.google.common.net.HttpHeaders.SET_COOKIE;
+
 public class RequestHandler implements Runnable {
     private static final Logger LOGGER = LoggerFactory.getLogger(RequestHandler.class);
-    private final UserController userController = new UserController();
+    private final FrontController frontController = new FrontController();
     private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -34,63 +40,63 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            HttpRequest httpRequest = new HttpRequest();
+            HttpRequest request = new HttpRequest();
+            HttpResponse response = new HttpResponse();
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)); // HTTP Request 읽기
-            String startLine = reader.readLine(); // start line
-            HttpRequestLine httpRequestLine = new HttpRequestLine(startLine);
-            httpRequest.setRequestLine(httpRequestLine);
-            LOGGER.debug("--- HTTP Request : {} ---", httpRequestLine);
+            String startLine = reader.readLine(); // request line
+            RequestLine requestLine = new RequestLine(startLine);
+            request.setRequestLine(requestLine);
+            LOGGER.debug("--- HTTP Request : {} ---", requestLine);
 
             String line;
             List<String> headers = new ArrayList<>();
-            while (!(line = reader.readLine()).equals("")) { // headers
+            while (!(line = reader.readLine()).isEmpty()) { // request headers
                 headers.add(line);
                 LOGGER.debug("{}", line);
             }
             HttpHeader header = new HttpHeader(headers);
-            httpRequest.setHeaders(header);
+            request.setHeaders(header);
 
-            if (header.contains("Content-Length")) { // body
-                int contentLength = Integer.parseInt(header.findFieldByName("Content-Length"));
+            if (header.contains(CONTENT_LENGTH)) { // request body
+                int contentLength = Integer.parseInt(header.findFieldByName(CONTENT_LENGTH));
                 char[] requestBody = new char[contentLength];
                 reader.read(requestBody, 0, contentLength);
-                StringBuilder builder = new StringBuilder();
-                builder.append(requestBody);
-                HttpBody body = new HttpBody(builder.toString());
-                httpRequest.setBody(body);
-                LOGGER.debug("Request Body : {}", builder);
+                String decode = URLDecoder.decode(String.valueOf(requestBody), StandardCharsets.UTF_8);
+                RequestBody body = new RequestBody(decode);
+                request.setBody(body);
+                LOGGER.debug("Request Body : {}", decode);
             }
+
             DataOutputStream dos = new DataOutputStream(out);
 
-            String url = userController.requestMapping(httpRequest);
-            if (url.startsWith("redirect")) {
-                response302Header(dos, url);
-                return;
+            frontController.service(request, response);
+
+            responseHeader(dos, response);
+            if (response.getBody() != null) {
+                responseBody(dos, response.getBody());
             }
-
-            byte[] body = HttpUtils.findFilePath(url);
-            response200Header(dos, body.length, HttpUtils.findContentType(url));
-            responseBody(dos, body);
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
+    private void responseHeader(DataOutputStream dos, HttpResponse response) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK\r\n");
-            dos.writeBytes("Content-Type: " + contentType + "\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos, String url) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 FOUND\r\n");
-            dos.writeBytes("Location: " + url.substring(url.indexOf("/")) + "\r\n");
+            dos.writeBytes(response.getStatusLine() + "\r\n");
+            HttpHeader headers = response.getHeaders();
+            if (headers.contains(CONTENT_TYPE)) {
+                dos.writeBytes(CONTENT_TYPE + ": " + headers.findFieldByName(CONTENT_TYPE) + "\r\n");
+            }
+            if (headers.contains(LOCATION)) {
+                dos.writeBytes(LOCATION + ": " + headers.findFieldByName(LOCATION) + "\r\n");
+            }
+            if (headers.contains(SET_COOKIE)) {
+                dos.writeBytes(SET_COOKIE + ": " + headers.findFieldByName(SET_COOKIE) + "\r\n");
+            }
+            if (response.getBody() != null) {
+                dos.writeBytes(CONTENT_LENGTH + ": " + response.getBody().length + "\r\n");
+            }
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
