1972b8550a2beae412361238fa08e7852cdf3f70
DOEKYONG <apochmn@naver.com>
2023-05-23 15:45:01 +0900

    feat : 세션 구현

--- src/main/java/util/request/HttpRequest.java
+++ src/main/java/util/request/HttpRequest.java
@@ -1,16 +1,17 @@
 package util.request;
 
-import java.util.Map;
-
 public class HttpRequest {
 
     private final String url;
 
     private final HttpHeader headers;
 
+    private final Session session;
+
     public HttpRequest(HttpHeader header) {
         this.url = parseUrl(header.getRequestLine());
         this.headers = header;
+        this.session = new Session();
     }
 
     private String parseUrl(String requestLine){
@@ -25,5 +26,9 @@ public class HttpRequest {
 
     public HttpHeader getHeaders() {
         return headers;
+    }
+
+    public Session Session() {
+        return session;
     }
 }
