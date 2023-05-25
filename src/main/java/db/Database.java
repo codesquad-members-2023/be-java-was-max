f3650fa29fe0b02d75e1fc029a5769a92964a138
sudago <sudago22@gmail.com>
2023-05-25 16:20:20 +0900

    fix: 동시성 이슈를 대비하여 수정

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 import java.util.Map;
 
 public class Database {
-    private static Map<String, User> users = Maps.newHashMap();
+    private static final Map<String, User> users = Maps.newConcurrentMap();
 
     public static String addUser(User user) {
         users.put(user.getUserId(), user);
