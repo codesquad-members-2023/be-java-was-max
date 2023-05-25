55ea650491a0e5427d471eeb55b4d07449a7bd84
Hana <dokkisan.wang@gmail.com>
2023-05-25 14:03:18 +0900

    refactor: logger 제거

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,17 +1,13 @@
-package webserver;
+package controller;
 
 import db.Database;
 import model.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class UserController {
-    private final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     public void join(Map<String, String> queryMap) {
         User user = new User(queryMap.get("userId"), queryMap.get("password"), queryMap.get("name"), queryMap.get("email"));
         Database.addUser(user);
-        logger.info(user.toString());
     }
 }
