37db36c9734754b230cf16b749f8b582ae9197df
Hana <dokkisan.wang@gmail.com>
2023-05-25 15:45:45 +0900

    refactor: 사용하지 않는 get 메서드 삭제

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -17,18 +17,6 @@ public class User {
         return userId;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
     @Override
     public String toString() {
         return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
