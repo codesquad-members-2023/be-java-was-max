b5be365e9b05cf0e7c857fb0bbeb431838d39744
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-05-31 20:50:45 +0900

    feat: ModelAndView 객체 추가
    - ModelAndView는 클라이언트에게 전달할 데이터와 뷰 정보를 저장하는 역할입니다.

--- src/main/java/webserver/frontcontroller/ModelAndView.java
+++ src/main/java/webserver/frontcontroller/ModelAndView.java
@@ -1,15 +1,13 @@
 package webserver.frontcontroller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ModelAndView {
 
     private String viewName;
-    private Map<String, Object> model = new HashMap<>();
+    private Model model;
 
-    public ModelAndView(String viewName) {
+    public ModelAndView(String viewName, Model model) {
         this.viewName = viewName;
+        this.model = model;
     }
 
     public String getViewName() {
@@ -20,11 +18,19 @@ public class ModelAndView {
         this.viewName = viewName;
     }
 
-    public Map<String, Object> getModel() {
+    public Model getModel() {
         return model;
     }
 
-    public void setModel(Map<String, Object> model) {
+    public void setModel(Model model) {
         this.model = model;
+    }
+
+    public boolean containsAttribute(String key) {
+        return model.containsAttribute(key);
+    }
+
+    public Object getAttribute(String key) {
+        return model.getAttribute(key);
     }
 }
