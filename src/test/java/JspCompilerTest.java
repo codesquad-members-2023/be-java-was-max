08a5b27f1cc8d498d28e554e9c02250c0b967f1f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-05-31 09:26:12 +0900

    chore: 메소드명 변경으로 인한 코드 정리
    - FileUtils.readFile -> FileUtils.getFileFromPath로 변경

--- src/test/java/JspCompilerTest.java
+++ src/test/java/JspCompilerTest.java
@@ -8,19 +8,17 @@ import webserver.jsp.JspCompiler;
 import webserver.jsp.JspServlet;
 
 import java.io.*;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 class JspCompilerTest {
 
     @Test
     @DisplayName("HttpResonse가 주어지고 jsp 파일을 요청했을때 jspServlet이 실행된 결과가 HttpResponse에 담기는지 테스트")
-    public void test4() throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, InterruptedException {
+    public void test1() throws IOException {
         // given
-        HttpRequest request = new HttpRequest(null, null, null, null, null);
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        HttpRequest request = new HttpRequest(null, new RequestHeader(new HashMap<>()), null, null, null);
         HttpResponse response = new HttpResponse();
-        File jspFile = FileUtils.readFile("/twoDice.jsp").orElseThrow();
+        File jspFile = FileUtils.getFileFromPath("/index.jsp").orElseThrow();
         // when
         JspServlet servlet = JspCompiler.service(jspFile);
         servlet.service(request, response);
@@ -28,18 +26,19 @@ class JspCompilerTest {
         OutputStreamWriter dos = response.getMessageBodyWriter();
         dos.close();
 
+        ByteArrayOutputStream bos = response.getMessageBodyOutputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(bos.toByteArray())));
         reader.lines().forEach(System.out::println);
     }
 
     @Test
     @DisplayName("index.jsp 파일을 요청했을때 비로그인 상태에서 실행 결과가 나오는지 테스트")
-    public void test5() throws IOException, ClassNotFoundException, InterruptedException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    public void test2() throws IOException {
         // given
         HttpRequest request = new HttpRequest(null, new RequestHeader(new HashMap<>()), null, null, null);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         HttpResponse response = new HttpResponse();
-        File jspFile = FileUtils.readFile("/index.jsp").orElseThrow();
+        File jspFile = FileUtils.getFileFromPath("/index.jsp").orElseThrow();
         // when
         JspServlet servlet = JspCompiler.service(jspFile);
         servlet.service(request, response);
