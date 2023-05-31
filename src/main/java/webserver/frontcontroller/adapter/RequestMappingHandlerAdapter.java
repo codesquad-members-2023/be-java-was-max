34d71851f08dd92b39946ee16607c50258b1de4f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-05-31 21:00:48 +0900

    fix: 세션 데이터를 ModelAndView 전달하도록 변경

--- src/main/java/webserver/frontcontroller/adapter/RequestMappingHandlerAdapter.java
+++ src/main/java/webserver/frontcontroller/adapter/RequestMappingHandlerAdapter.java
@@ -2,8 +2,9 @@ package webserver.frontcontroller.adapter;
 
 import http.request.HttpRequest;
 import http.response.HttpResponse;
-import java.util.HashMap;
-import java.util.Map;
+import http.session.HttpSession;
+import http.session.SessionContainer;
+import webserver.frontcontroller.Model;
 import webserver.frontcontroller.ModelAndView;
 import webserver.frontcontroller.controller.Handler;
 import webserver.frontcontroller.controller.RequestMappingHandler;
@@ -18,16 +19,20 @@ public class RequestMappingHandlerAdapter implements MyHandlerAdapter {
     @Override
     public ModelAndView handle(HttpRequest request, HttpResponse response, Object handler) {
         Handler controller = (Handler) handler;
-        Map<String, Object> model = new HashMap<>();
-        String viewName = controller.process(request, response);
-        ModelAndView mv = new ModelAndView(viewName);
+        Model model = new Model();
+        String viewName = controller.process(request, response, model);
+        HttpSession session = SessionContainer.getSession(request.getSessionId());
+        // 세션에 있는 데이터들을 model 객체로 복사
+        sessionToModel(session, model);
 
         if (viewName.startsWith("redirect:")) {
             viewName = viewName.replace("redirect:", "");
-            mv.setViewName(viewName);
-            model.put("redirect", Boolean.TRUE);
+            model.addAttribute("redirect", Boolean.TRUE);
         }
-        mv.setModel(model);
-        return mv;
+        return new ModelAndView(viewName, model);
+    }
+
+    private void sessionToModel(HttpSession session, Model model) {
+        session.keySet().forEach(key -> model.addAttribute(key, session.getAttribute(key)));
     }
 }
